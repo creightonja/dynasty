6d7bd80de43b6cfa366e6f80cd25d5a53ec7b206
Kenny Grage <kennygrage@gmail.com>
2017-07-29 16:46:17 -0700

    home view

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+
+    def index
+        render :template => "home/index"
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   resources :users
   resources :addresses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'home#index'
 end
